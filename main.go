9217d689aa6a0e35e4e6a8248f99c94905e6cfe2
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-11-09 17:57:55 +0530

    switched over to using local generated stubs for project-planton apis

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	confluentkafkav1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/confluent/confluentkafka/v1"
 	"github.com/pkg/errors"
 	"github.com/project-planton/confluent-kafka-pulumi-module/pkg"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
+	confluentkafkav1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/confluent/confluentkafka/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
